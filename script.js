e2b47d4e7e0df729fd7733d6d6be4a763c71dedb
Alex Nascimento <alexbrazuca@live.com>
2020-12-04 04:17:03 -0300

    Pagina de contato

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ document.querySelector(".hamburguer").addEventListener("click", ()=>
     document.querySelector(".container").classList.toggle("show-menu")
 );
 
+var text = '{"employees":['+
+'{"firstName":"John","lastName":"Doe"},'+
+'{"firstName":"John","lastName":"Doe"},'+
+'{"firstName":"John","lastName":"Doe"}]}';
+
+console.log(text);
